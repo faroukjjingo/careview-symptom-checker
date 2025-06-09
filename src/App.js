0f0288f5936cf812351803f8621c1cd0e2680d5e
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-09 06:49:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
 import Checker from './components/Checker';
 import './App.css';
 
 function App() {
   return (
-    <Container className="app-container">
+    <div className="app-container">
       <Checker />
-    </Container>
+    </div>
   );
 }
 
