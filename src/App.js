2257a0cf922c6da575a109e37ea924de143c5b5a
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-06 18:37:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Checker from './components/Checker';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 import './index.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-background text-foreground font-sans">
-      <Checker />
+    <div className="min-h-screen bg-background text-foreground font-sans flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-4">
+          <Checker />
+        </main>
+      </div>
     </div>
   );
 }
