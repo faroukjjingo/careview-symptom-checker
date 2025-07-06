300f939945d364b82b39a4b1521f7aa8056ca719
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-06 21:09:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+// src/App.jsx
 import React, { useState } from 'react';
 import Checker from './components/Checker';
 import Header from './components/Header';
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="min-h-screen bg-background text-foreground font-sans flex flex-col">
       <Header isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-      <div className="flex flex-1">
-        <main className="flex-1 p-4 md:p-6">
+      <div className="flex flex-1 overflow-hidden">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
           <Checker />
         </main>
       </div>
