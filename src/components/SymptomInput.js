310d8ae07f2af24ac20ada39d1937173cef0f2b5
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-06 16:40:41 +0300

    Update SymptomInput.js

--- src/components/SymptomInput.js
+++ src/components/SymptomInput.js
@@ -4,7 +4,7 @@ import { symptomList } from './SymptomList';
 import { symptomCombinations } from './SymptomCombinations';
 import { travelRiskFactors } from './TravelRiskFactors';
 import { riskFactorWeights } from './RiskFactorWeights';
-import { drugHistoryWeights } from './DrugHistoryWeights';
+import drugHistoryWeights from './DrugHistoryWeights';
 
 const SymptomInput = ({ onDiagnosisResults }) => {
   const [input, setInput] = useState('');
